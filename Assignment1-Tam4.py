fd8b95ebfb49a4df88d1eca3e0f7db1683def77f
Masoume Bayat <111157102+MasoumeBayat@users.noreply.github.com>
2022-08-13 16:57:32 +0430

    Update Assignment1-Tam4.py

--- Assignment1-Tam4.py
+++ Assignment1-Tam4.py
@@ -1,4 +1,4 @@
-lenght= int(input("Please enter the length of one side of the triangle:"))
-Height=int(input("Please enter the height of the triangle:"))
+lenght= float(input("Please enter the length of one side of the triangle:"))
+Height=float(input("Please enter the height of the triangle:"))
 area=(lenght*Height)/2
 print("area= ",area)
